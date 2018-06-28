3002685a1c367ae649afc634e3dd439851a82220
ffont <frederic.font@upf.edu>
2018-06-28 13:49:38 +0200

    Add Essentia music extractor and fake JSONLD output

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,96 @@
 FROM python:3
 
+# Common requirements
 RUN apt-get update \
-    && apt-get install -y libyaml-0-2 libfftw3-3 libtag1v5 libsamplerate0 \
-       libavcodec57 libavformat57 libavutil55 \
-       libavresample3 python3 python3-numpy libpython3.5 python3-yaml python3-six libsndfile1 \
+    && apt-get install -y \
+        libyaml-0-2 \ 
+        libfftw3-3 \ 
+        libtag1v5 \ 
+        libsamplerate0 \
+        libavcodec57 \ 
+        libavformat57 \ 
+        libavutil55 \
+        libavresample3 \ 
+        python3 \ 
+        python3-numpy \ 
+        libpython3.5 \ 
+        python3-yaml \ 
+        python3-six \
+        libsndfile1 \
+        pkg-config \
+        swig \
 	&& rm -rf /var/lib/apt/lists/*
 
-RUN pip install numpy soundfile librosa scipy
-RUN git clone https://github.com/AudioCommons/timbral_models.git && cd timbral_models && python setup.py install
 
+# Python dependencies (needed for essentia)
+RUN pip install numpy==1.14.5
+
+
+# Gaia
+# See https://github.com/MTG/gaia
 RUN apt-get update \
-    && apt-get install -y build-essential libyaml-dev libfftw3-dev \
-       libavcodec-dev libavformat-dev libavutil-dev libavresample-dev \
-       python-dev libsamplerate0-dev libtag1-dev python3-numpy-dev git \
+    && apt-get install -y \
+        build-essential \
+        python \
+        libqt4-dev \
+        libyaml-dev \
+        python-dev \
+    && git clone https://github.com/MTG/gaia /tmp/gaia \
+    && cd /tmp/gaia \
+    && python2 ./waf configure \
+    && python2 ./waf \
+    && python2 ./waf install \
+    && cd / && rm -r /tmp/gaia
+
+
+# Essentia
+RUN apt-get update \
+    && apt-get install -y \
+        build-essential \ 
+        libyaml-dev \
+        libfftw3-dev \
+        libavcodec-dev \
+        libavformat-dev \
+        libavutil-dev \
+        libavresample-dev \
+        python-dev \
+        libsamplerate0-dev \
+        libtag1-dev \
+        python3-numpy-dev \
+        git \
     && mkdir /essentia && cd /essentia && git clone https://github.com/MTG/essentia.git \
-    && cd /essentia/essentia && git checkout 76142076e563173397349517bff15c9fe54cb163 \
-    && ./waf configure --with-examples --with-python \
+    && cd /essentia/essentia && git checkout 74b355fc14fe48f59bdff9608dfe5ce3c54640a4 \ 
+    && ./waf configure --with-examples --with-python --with-gaia \
     && ./waf && ./waf install && ldconfig \
-    &&  apt-get remove -y build-essential libyaml-dev libfftw3-dev libavcodec-dev \
-        libavformat-dev libavutil-dev libavresample-dev python3-dev libsamplerate0-dev \
-        libtag1-dev python3-numpy-dev \
+    &&  apt-get remove -y \
+        build-essential \
+        libyaml-dev \
+        libfftw3-dev \
+        libavcodec-dev \
+        libavformat-dev \
+        libavutil-dev \
+        libavresample-dev \
+        python3-dev \
+        libsamplerate0-dev \
+        libtag1-dev \
+        python3-numpy-dev \
     && apt-get autoremove -y \
     && apt-get clean -y \
     && rm -rf /var/lib/apt/lists/* \
     && cd / && rm -rf /essentia/essentia
 
+# Extra python dependencies
+RUN pip install SoundFile==0.10.2 librosa==0.6.1 scipy==1.1.0
+RUN pip install rdflib==4.2.2 rdflib-jsonld==0.4.0 PyLD==1.0.3
+RUN git clone https://github.com/AudioCommons/timbral_models.git && cd timbral_models && python setup.py install
+
+
+# Add high-level models and music extractor configuration
+RUN mkdir -p models
+ADD models /models
+ADD music_extractor_profile.yaml /
+
+
+# Add analysis script
 ADD analyze.py /
 ENTRYPOINT [ "python", "/analyze.py" ]
--- analyze.py
+++ analyze.py
@@ -4,13 +4,13 @@ import json
 import math
 import numpy as np
 import essentia
+essentia.log.infoActive = False
+essentia.log.warningActive = False
 import logging
 from argparse import ArgumentParser
-from essentia.standard import FreesoundExtractor, YamlOutput, LoopBpmConfidence, PercivalBpmEstimator, EasyLoader, PitchContourSegmentation, PredominantPitchMelodia
-
+from essentia.standard import MusicExtractor, FreesoundExtractor, YamlOutput, LoopBpmConfidence, PercivalBpmEstimator, EasyLoader, PitchContourSegmentation, PredominantPitchMelodia
 
 logger = logging.getLogger()
-
 
 ac_mapping = {
     "ac:duration": "metadata.audio_properties.length",
@@ -27,11 +27,8 @@ ac_mapping = {
 }
 
 def run_freesound_extractor(audiofile):
-    logger.debug('{0}: running Freesound Extractor'.format(audiofile))
+    logger.debug('{0}: running Essentia\'s FreesoundExtractor'.format(audiofile))
 
-    # Disable Essentia logging and run extractor
-    essentia.log.infoActive = False
-    essentia.log.warningActive = False
     fs_pool, _ = FreesoundExtractor()(audiofile)
     return fs_pool
 
@@ -92,7 +89,7 @@ def ac_pitch_description(audiofile, fs_pool, ac_descriptors):
     ac_descriptors["ac:note_confidence"] = float(fs_pool['lowlevel.pitch_instantaneous_confidence.median'])
 
 
-def ac_timbral_models(audiofile, fs_pool, ac_descriptors):
+def ac_timbral_models(audiofile, ac_descriptors):
     logger.debug('{0}: computing timbral models'.format(audiofile))
 
     # TODO: update to latest version of timbral descriptors: https://github.com/AudioCommons/timbral_models/issues/5#issuecomment-376178206
@@ -112,7 +109,73 @@ def ac_timbral_models(audiofile, fs_pool, ac_descriptors):
             value = 0
         ac_descriptors[name] = value
 
-def analyze(audiofile, jsonfile, compute_timbral_models=False):
+
+def ac_highlevel_music_description(audiofile, ac_descriptors):
+    logger.debug('{0}: running Essentia\'s MusicExtractor'.format(audiofile))
+    me_pool, _ = MusicExtractor(profile='music_extractor_profile.yaml')(audiofile)
+    ac_descriptors["ac:genre"] = me_pool['highlevel.genre_test.value']
+    ac_descriptors["ac:mood"] = me_pool['highlevel.mood_test.value']
+
+
+def render_jsonld_output(ac_descriptors):
+    # NOTE: this is a fake implementation that renders arbitrary JSON-LD data
+
+    import rdflib,pyld
+    from rdflib import Graph, URIRef, BNode, Literal, Namespace, plugin
+    from rdflib.serializer import Serializer
+    from rdflib.namespace import RDF, FOAF
+    from pprint import pprint
+
+    def dlfake(input):
+        '''This is to avoid a bug in PyLD (should be easy to fix and avoid this hack really..)'''
+        return {'contextUrl': None,'documentUrl': None,'document': input}
+
+    g = Graph()
+    AC = Namespace("http://audiocommons.org/vocab/")
+    MO = Namespace("http://musicontology.com/")
+    DC = Namespace("http://purl.org/dc/terms/")
+    response_stats = BNode()
+    g.add((response_stats, RDF.type, AC.AggregatedResponseStats))
+    g.add((response_stats, AC.sent_timestamp, Literal("2016-12-22 16:58:55.128886")))
+    g.add((response_stats, AC.current_timestamp, Literal("2016-12-22 16:58:55.158931")))
+    g.add((response_stats, AC.n_received_responses, Literal(5)))  # Add 1 for simulated error response
+    g.add((response_stats, AC.response_status, Literal("FI")))
+    g.add((response_stats, AC.response_id, Literal("9097e3bb-2cc8-4f99-89ec-2dfbe1739e67")))
+    g.add((response_stats, AC.collect_url, Literal("https://m.audiocommons.org/api/v1/collect/?rid=9097e3bb-2cc8-4f99-89ec-2dfbe1739e67")))
+
+    context = {
+        "rdf": str(RDF),  #  This might not be needed if we only use RDF.type
+        "ac": str(AC),
+        "mo": str(MO),
+        "dc": str(DC),
+    }
+
+    '''
+    frame = {
+      #"@context": context,  # I think this is not needed
+      "@type": str(AC.AggregatedResponse),
+      str(AC.stats): {
+        "@type": str(AC.AggregatedResponseStats),
+      },
+      str(AC.contents): {
+        "@type": str(AC.IndividualResponse),
+      },
+      str(AC.errors): {
+        "@type": str(AC.IndividualErrorResponse),
+      },
+      str(AC.warnings): {
+        "@type": str(AC.IndividualWarningsResponse),
+      },
+    }
+    '''
+    frame = {"@type": str(AC.AggregatedResponse)}  # Apparently just by indicating the frame like this it already builds the desired output
+    jsonld = g.serialize(format='json-ld', context=context).decode() # this gives us direct triple representation in a compact form
+    jsonld = pyld.jsonld.frame(jsonld, frame, options={"documentLoader":dlfake}) # this "frames" the JSON-LD doc but it also expands it (writes out full URIs)
+    jsonld = pyld.jsonld.compact(jsonld, context, options={"documentLoader":dlfake}) # so we need to compact it again (turn URIs into CURIEs)
+    return jsonld
+
+
+def analyze(audiofile, outfile, compute_timbral_models=False, compute_highlevel_music_descriptors=False, out_format="json"):
     logger.info('{0}: starting analysis'.format(audiofile))
 
     # Get initial descriptors from Freesound Extractor
@@ -125,10 +188,19 @@ def analyze(audiofile, jsonfile, compute_timbral_models=False):
     ac_tempo_description(audiofile, fs_pool, ac_descriptors)
     ac_pitch_description(audiofile, fs_pool, ac_descriptors)
     if compute_timbral_models:
-        ac_timbral_models(audiofile, fs_pool, ac_descriptors)
+        ac_timbral_models(audiofile, ac_descriptors)
+    if compute_highlevel_music_descriptors:
+        ac_highlevel_music_description(audiofile, ac_descriptors)
+    
+    if out_format == 'jsonld':
+        # Convert output to JSON-LD
+        output = render_jsonld_output(ac_descriptors)
+    else:
+        # By default (or in case of unknown format, use JSON)
+        output = ac_descriptors
 
     logger.info('{0}: analysis finished'.format(audiofile))
-    json.dump(ac_descriptors, open(jsonfile, 'w'), indent=4)
+    json.dump(output, open(outfile, 'w'), indent=4)
 
     
 if __name__ == '__main__':
@@ -136,11 +208,14 @@ if __name__ == '__main__':
     AudioCommons audio extractor (v2). Analyzes a given audio file and writes results to a JSON file.
     """)
     parser.add_argument('-v', '--verbose', help='if set prints more info on screen', action='store_const', const=True, default=False)
-    parser.add_argument('-t', '--timbral_models', help='if set, compute timbral models as well', action='store_const', const=True, default=False)
+    parser.add_argument('-t', '--timbral-models', help='if set, compute timbral models as well', action='store_const', const=True, default=False)
+    parser.add_argument('-m', '--music-highlevel', help='if set, compute high-level music descriptors', action='store_const', const=True, default=False)
     parser.add_argument('-i', '--input', help='input audio file', required=True)
-    parser.add_argument('-o', '--output', help='output json file', required=True)
+    parser.add_argument('-o', '--output', help='output analysis file', required=True)
+    parser.add_argument('-f', '--format', help='format of the output analysis file ("json" or "jsonld", defaults to "jsonld")', default="json")
     
     args = parser.parse_args()
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO if not args.verbose else logging.DEBUG)
-    analyze(args.input, args.output, args.timbral_models)
+
+    analyze(args.input, args.output, args.timbral_models, args.music_highlevel, args.format)
     
